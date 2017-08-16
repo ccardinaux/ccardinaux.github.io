d12b07e4cdda8506c410fe8470beee7fc9a536e4
Charles Cardinaux <v-carcha@microsoft.com>
2017-08-16 14:42:37 -0700

    Add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
+const path = require('path');
 const gulp = require('gulp');
 const child = require('child_process');
 const gutil = require('gulp-util');
 const eslint = require('gulp-eslint');
+const stylelint = require('gulp-stylelint');
+const sass = require('gulp-sass');
+const minifycss = require('gulp-uglifycss');
+const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
 gulp.task('eslint', () => {
@@ -9,6 +14,32 @@ gulp.task('eslint', () => {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});
+
+// Lint Sass
+gulp.task('stylelint', () => {
+  gulp.src('_sass/**/*.scss')
+    .pipe(stylelint({
+      reporters: [
+        {
+          formatter: 'string',
+          console: true,
+        },
+      ],
+    }));
+});
+
+// Compile and minify Sass
+gulp.task('sass', ['stylelint'], () => {
+  gulp.src('_sass/main.scss')
+    .pipe(sass({
+      includePaths: [
+        path.join(__dirname, '/node_modules/normalize.css/'),
+      ],
+    }))
+    .pipe(autoprefixer(['last 2 versions']))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./assets'));
 });
 
 gulp.task('jekyll', () => {
@@ -40,7 +71,7 @@ gulp.task('serve', () => {
     },
   });
 
-  // add watch task here
+  gulp.watch('_sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['eslint', 'jekyll', 'serve']);
+gulp.task('default', ['sass', 'jekyll', 'serve']);
